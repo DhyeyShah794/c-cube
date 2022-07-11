8ed6dacf65c68690477d118b52f61b83175783b8
Dhyey Shah <83686569+DhyeyShah794@users.noreply.github.com>
2022-07-11 23:49:15 +0530

    Phone input format

--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -1,74 +1,98 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Nav from '../../Components/Navbar/navbar';
 import Footer from '../../Components/Footer/footer';
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+// import { isPossiblePhoneNumber } from 'react-phone-number-input';
 import './contact.css';
 
+let phoneNumber = '';
 
 const Contact = () => {
+    const [value, setValue] = useState()
+    const [message, setMessage] = useState('')
+
+    const handleChange = event => {
+        setMessage(event.target.value);
+        console.log(value);
+    };
+
+    const handleClick = event => {
+        event.preventDefault();
+        console.log(message);
+    };
     return (
         <div className="contact">
             <Nav />
 
             <h1>Contact Us</h1>
 
-            <div className="form-data">
-            <table className="contact-form">
-            <tbody>
-                <tr>
-                    <td className="asked-info">
-                        Full Name:
-                    </td>
-                    <td>
-                        <input className="contact-us-text response" id="fullname" type="text" placeholder="Enter your full name" required />
-                    </td>
-                </tr>
+            <div className="form-container">
+                <table className="contact-table">
+                    <tbody>
+                        <tr>
+                            <td className="asked-info">
+                                Full Name:
+                            </td>
+                            <td>
+                                <input onChange={handleChange} className="response" id="full-name" type="text" placeholder="Enter your full name" required />
+                            </td>
+                        </tr>
 
-                <tr>
-                    <td className="asked-info">
-                        Contact Number:
-                    </td>
-                    <td>
-                        <input id ="conno" type="tel" className="input-box response" placeholder="0123456789" required/>
-                    </td>
-                </tr>
-                
-                <tr>
-                  <td className="asked-info">Date of Birth:</td>
-                  <td><input id ="dob" type="date" className="input-box response" placeholder="DD/MM/YYYY" required/></td>
-                </tr>
+                        <tr>
+                            <td className="asked-info">
+                                Contact Number:
+                            </td>
+                            <td>
+                                <PhoneInput placeholder="Enter phone number" limitMaxLength={true} defaultCountry="IN" value={value} onChange={setValue} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="asked-info">
+                                Email Address:
+                            </td>
+                            <td>
+                                <input onChange={handleChange} className="response" id="email" type="text" placeholder="Enter your email address" required />
+                            </td>
+                        </tr>
 
-                <tr>
-                    <td className="asked-info">
-                        Address:
-                    </td>
-                    <td>
-                        <input className="contact-us-text response" id="addr" type="text" placeholder="Flat no, Apartment Name,
-                        Landmark, Area, City, Pincode ." required />
-                    </td>
-                </tr>
+                        <tr>
+                            <td className="asked-info">Date of Birth:</td>
+                            <td><input onChange={handleChange} className="response" id="dob" type="date" placeholder="" required /></td>
+                        </tr>
 
-                <tr>
-                    <td className="asked-info">
-                        Why do you wish to volunteer here?
-                    </td>
-                    <td>
-                        <input id="volinfo" type="text" className="input-box response" placeholder="State your reason to volunteer here" required/>
-                    </td>
-                </tr>
+                        <tr>
+                            <td className="asked-info">
+                                Address:
+                            </td>
+                            <td>
+                                <input onChange={handleChange} className="response" id="address" type="text" placeholder="Flat no, Apartment Name,
+                                Landmark, Area, City, Pincode ." required />
+                            </td>
+                        </tr>
 
-                <tr>
-                  <td className="asked-info">Have you volunteered at any other Foundation or NGO?</td>
-                  <td>
-                    <select className="response" id="prevvol">
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                    </select></td>
-                </tr>
+                        <tr>
+                            <td className="asked-info">
+                                Why do you wish to volunteer here?
+                            </td>
+                            <td>
+                                <input onChange={handleChange} className="response" id="vol-info" type="text" placeholder="State reason for volunteering" required />
+                            </td>
+                        </tr>
 
-            </tbody>
-            </table>
+                        <tr>
+                            <td className="asked-info">Have you volunteered at any other Foundation or NGO?</td>
+                            <td>
+                                <select className="response" id="prev-vol">
+                                    <option value="yes">Yes</option>
+                                    <option value="no">No</option>
+                                </select></td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
                 <div>
-                    <button id="sendbtn">Send</button>
+                    <button onClick={handleClick} id="send-btn">Send</button>
                 </div>
             </div>
 
