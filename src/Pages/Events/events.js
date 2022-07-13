27462d454cec6b6be9d84e609e904e2120303b81
Nisarg Shah <devnisargshah@gmail.com>
2022-07-13 11:44:09 +0530

    added post and posts, changed events

--- src/Pages/Events/events.js
+++ src/Pages/Events/events.js
@@ -1,93 +1,36 @@
-import React from 'react';
-import Nav from '../../Components/Navbar/navbar';
-import Footer from '../../Components/Footer/footer';
-import './events.css';
-
+import React from "react";
+import Nav from "../../Components/Navbar/navbar";
+import Footer from "../../Components/Footer/footer";
+import Posts from "../../Components/Posts/Posts";
+import "./events.css";
 
 const Events = () => {
-    return (
-        <div classNameName="events">
-            <Nav />
-            <div className="marquee">
-                <div className="marquee-content">
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image1.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image2.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image3.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image4.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image5.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image6.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image1.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image2.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image3.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image4.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image5.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image6.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image1.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image2.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image3.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image4.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image5.jpg")} alt="" />
-                    </div>
-
-                    <div className="marquee-item">
-                        <img src={require("../../Components/Images/image6.jpg")} alt="" />
-                    </div>
-
-                </div>
-            </div>
-            <Footer />
-        </div>
-    );
-}
+  return (
+    <div classNameName="events">
+      <Nav />
+      <div className="events-intro">
+        <h3 className="events-heading"><strong>Events</strong></h3>
+        <p className="events-intro-info">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+        </p>
+      </div>
+      <Posts />
+      <Footer />
+    </div>
+  );
+};
 
 export default Events;
