472a86caca6200d2a10d000ada3470b4fe3f9de1
Dhyey Shah <83686569+DhyeyShah794@users.noreply.github.com>
2022-07-21 02:08:41 +0530

    UI Changes

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css'
 import './navbar.css';
 
@@ -8,13 +8,11 @@ const NavigationBar = () => {
 
     return (
         <Container className="nav-container">
+            <Nav.Link href="/"><img className="logo" src={require("../Images/C-Cube.png")} alt="C-cube" /></Nav.Link>
             <Navbar className="nav-bar" bg="nav-bar" variant="dark" fixed="top" expand="sm" collapseOnSelect>
-                <Navbar.Brand className="nav-brand">
-                    <Nav.Link href="/"><img className="logo" src={require("../Images/C-Cube.png")} alt="C-cube" /></Nav.Link>
-                </Navbar.Brand>
-
                 <Navbar.Toggle className="nav-toggle" aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className="nav-collapse">
+
                     <Nav className="nav">
                         <Nav.Link className="nav-link" href="/">Home</Nav.Link>
                         <Nav.Link className="nav-link" href="/about">About</Nav.Link>
--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -8,38 +8,55 @@ const About = () => {
     return (
         <div className="about">
             <Nav />
-            <h1 className="atitle">C-CUBE :- CHARACTER CULTURE COMPETENCE </h1>
-            <p className="info">The main motto of this club is to train today's youths to become great leaders in the future Intellectual education influences the head and value-based education influences the heart. In fact, education that does not train the heart can be dangerous. If we want to build character in our offices, homes and society, we must have the wisdom to live a principle-centered life based on honesty, compassion, courage, responsibility etc. Educated persons are those who can choose wisely and courageously under any circumstances between good and bad, between virtuousness and vulgarities regardless of the academic degrees they have. The goal of C-Cube is to rekindle the innate wisdom about meaning and purpose of life</p>
-            
-            <h2> GOALS </h2>
+            <div className="a-title">
+                <h1><strong>C-Cube</strong></h1>
+                <div className="box-2">
+                    <h2>Culture | Character | Competence</h2>
+                    <div className="filled-2"></div>
+                </div>
+            </div>
+            <p className="info">The main motto of this club is to train today's youths to become great leaders in the future Intellectual education influences the head and value-based education influences the heart. In fact, education that does not train the heart can be dangerous. If we want to build character in our offices, homes and society, we must have the wisdom to live a principle-centered life based on honesty, compassion, courage, responsibility etc.
+                <br /><br />
+                Educated persons are those who can choose wisely and courageously under any circumstances between good and bad, between virtuousness and vulgarities regardless of the academic degrees they have. The goal of C-Cube is to rekindle the innate wisdom about meaning and purpose of life</p>
 
-            <p className="info"> 1. Training in soft skills.<br />
-                2. Bringing out the potential in the individual.<br />
-                3. Blending Character with Competence in bringing
-                about long-enduring success.<br />
-                4. Balancing Personal vs Professional life and Work Vs.
-                Entertainment. <br />
-                5. Saving youth from self-destructive habits and
-                addictions that harm body and mind. <br />
-                6. Dealing with sensitivity and self awareness, in
-                thought, word and action.<br />
-                7. Webinars with international faculty for
-                broadening perspectives.<br />
-                8. Organizing outings for experiencing closer touch with
-                nature and marvelous gifts of Supreme Divinity. <br /></p>
-            
-            <h2>Mr. Vishal Kaushal</h2>
+            <div className="box-3">
+                <h2 style={{ marginTop: "7rem" }}> Our Goals </h2>
+                <div className="filled-3"></div>
+            </div>
 
-            <p className="info">1. Recipient of Institute Silver Medal from IIT Kharagpur. <br />
-                    2. Worked as Project Leader at Oracle Bangalore for 6 years.<br />
-                    3. Currently pursuing PhD from IIT Bombay. <br />
-                    4. IIT KGP Topper in Computer Science Engineering Department. <br />
-                    5. District Topper in CBSE in 12th.<br />
-                    6. Adjudged Second Best Student in the Institute across all branches and departments based on merit.<br />
-                    7. Won J.C.Ghosh Memorial Prize for being topper</p><br />
+            <p className="info">
+                <ol>
+                    <li>Training in soft skills</li>
+                    <li>Bringing out the potential in the individual</li>
+                    <li>Blending Character with Competence in bringing about long-enduring success</li>
+                    <li>Balancing Personal vs Professional life and Work Vs. Entertainment</li>
+                    <li>Saving youth from self-destructive habits and addictions that harm body and mind</li>
+                    <li>Dealing with sensitivity and self awareness, in thought, word and action</li>
+                    <li>Webinars with international faculty for broadening perspectives</li>
+                    <li>Organizing outings for experiencing closer touch with nature and marvelous gifts of Supreme Divinity</li>
+                </ol>
+            </p>
 
-            
-                
+            <div className="box-4">
+                <h2 style={{ color: "#3457D5" }}>Chairperson - Mr. Vishal Kaushal</h2>
+                <div className="filled-4"></div>
+                <img src={require("../../Components/Images/chairman.png")} alt="Vishal Kaushal" className="chairman-img" />
+            </div>
+
+            <p className="info">
+                <ul>
+                    <li>Recipient of Institute Silver Medal from IIT Kharagpur</li>
+                    <li>Worked as Project Leader at Oracle Bangalore for 6 years</li>
+                    <li>Currently pursuing PhD from IIT Bombay</li>
+                    <li>IIT KGP Topper in Computer Science Engineering Department</li>
+                    <li>District Topper in CBSE in 12th</li>
+                    <li>Adjudged Second Best Student in the Institute across all branches and departments based on merit</li>
+                    <li>Won J.C.Ghosh Memorial Prize for being topper</li>
+                </ul>
+            </p>
+
+
+
             <Footer />
         </div>
     );
