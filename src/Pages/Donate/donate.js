02fb9c33276a7b90cb423a9f7097e45a4d2da516
Pruthviraj Patil <pruthviraj.p@somaiya.edu>
2022-07-27 00:12:05 +0530

    donation page edited

--- src/Pages/Donate/donate.js
+++ src/Pages/Donate/donate.js
@@ -43,36 +43,33 @@ const Donate = () => {
             <h2 className="d-title"><strong>Donation</strong></h2>
             <div className="donation-info">
                 <p className="d-text">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem veniam ipsum culpa voluptates ratione
-                    suscipit nesciunt eius facilis illo distinctio explicabo sapiente voluptate beatae aspernatur, ex qui
-                    tempora quibusdam.
-                </p>
-                <p className="d-text">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quidem eaque voluptates excepturi in culpa ab,
-                    voluptatibus corrupti earum, vel provident, officiis rerum facilis cupiditate! Id dolorum quaerat unde
-                    deserunt.<br /> This will also benefit the community which we<br />
+                    This club is for Holistic development of youth. College
+education usually harps on the
+first two aspects, while
+association with an organization
+such as C-Cube helps in fulfilling
+the remaining one so show your support for the club by donating !
+                    Your small contribution would count to a huge contribution and growth of the organisation and learning of individuals.
+                
+                  This will also benefit the community which we
                     serve and together we can bring smile on a
                     lot of people's faces.
                 </p>
+                
             </div>
 
             <div class="info">
                 <h2>How to Donate?</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus fugiat velit eius, esse iure, nemo animi
-                        aliquid labore, quibusdam possimus sunt nam! Perspiciatis vitae illo nostrum culpa quis earum officiis
-                        ipsum ducimus dicta cumque possimus consequuntur, ad ratione laudantium provident a iusto mollitia
-                        repudiandae perferendis ullam nemo accusantium soluta?
+                    <p>If you want to donate follow the read this before filling the form .
                         <br /><br />
-                        Dolore minus amet quisquam nostrum voluptatem
-                        consequatur recusandae rerum tempora magni dolorum neque ipsa optio alias, perferendis nemo explicabo
-                        laudantium voluptas nesciunt maiores. Nostrum veritatis pariatur nihil doloremque iure et ratione, est a
-                        obcaecati! Suscipit, quam magnam inventore delectus ullam consequatur explicabo nemo ipsum nulla id
-                        tenetur numquam vero, exercitationem ex!
+                      Fill all your specified details in the form and make sure a proper email address is provided and submit it .
+                      <br /><br />
+                      You will recieve a mail with a link to pay which would be safe and secure to continue the transaction.
                     </p>
             </div>
-            {/* <div class="img-container">
+            <div class="img-container">
                 <img src={require("../../Components/Images/image2.jpg")} alt="Image1" class="donate-img"></img>
-            </div> */}
+            </div>
 
             <div class="form-data">
             <form className="form" onSubmit={handleSubmit}>
@@ -91,7 +88,8 @@ const Donate = () => {
                     <textarea className="response" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} required></textarea>
 
                     <button type="submit" className="send-btn" style={{background : loader ? "rgba(10, 10, 10, 0.5)" : "rgba(0, 0, 0, 0)"}}>Submit</button>
-
+                    
+                
                 </form>
             </div>
             <Footer />
